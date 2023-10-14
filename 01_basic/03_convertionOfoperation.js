829073224cf0ccb421105ba30e8bf65e3281ff9d
Kacha Darshan DK98 <112555093+kachadarshan@users.noreply.github.com>
2023-10-14 11:39:36 +0000

    operator

--- 01_basic/03_convertionOfoperation.js
+++ 01_basic/03_convertionOfoperation.js
@@ -4,13 +4,13 @@ let score ="33";                 //undefinde convert value is NaN
 
 let resu="33abc"
 
-console.log(typeof score);      //typeof show varible type
+//console.log(typeof score);      //typeof show varible type
 
 //now converstion of vsarible 
 
 let converted =Number(resu);    //Here Number is conver scroe varible type in int  //in resu value was not in number format still convertin number 
-console.log(typeof converted);
-console.log(converted);         //print value is NaN 
+//console.log(typeof converted);
+//console.log(converted);         //print value is NaN 
 
 
 //for data type Boolean 
@@ -24,4 +24,50 @@ console.log(converted);         //print value is NaN
 let numsum =33
 
 let stingnum =String(numsum)  //conver of string
-console.log(typeof stingnum)
+//console.log(typeof stingnum)
+
+
+
+/*----------------------------------------OPERATIONS----------------------------------------------------------------------------- */
+
+let value =3
+let negvalue =-value
+
+//console.log(negvalue)
+
+// console.log(2+2)                 //Addition Arethmetic Operation
+// console.log(2-2)                 //Substraction Arethmetic Operation
+// console.log(2*2)                 //Multiplication Arethmetic Operation when used ** multiplication used in power of any value 
+// console.log(2/2)                 //Division Arethmetic Operation
+
+
+//two string added 
+
+let str1 ="Darshan"
+let str2 =" Kacha"
+
+let str3 =str1+str2
+//console.log(str3)
+
+
+/*Now Complex part of Added one Datatype is String And Int */
+let str ="12"  //its is string type
+let srr = 3    //its is a int type
+
+let saa= str+srr
+//console.log(saa) 
+
+
+
+/*any value for auto increment */
+let gamecounter =100;
+gamecounter++;
+
+//Prefix Opertor And Postfix Opertaor learn
+
+console.log(gamecounter);
+
+
+
+//Link To Study MDN 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
