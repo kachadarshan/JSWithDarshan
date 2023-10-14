5f851448d4963fc5a76cfb878394b55eb074b1e2
Kacha Darshan DK98 <112555093+kachadarshan@users.noreply.github.com>
2023-10-14 10:36:47 +0000

    Conversion of datatypes

---
+++ 01_basic/03_convertionOfoperation.js
@@ -0,0 +1,27 @@
+let score ="33";                 //undefinde convert value is NaN
+                                 //null convert value is zero
+                                 //mixvalue convert value is NaN (Not A Number)
+
+let resu="33abc"
+
+console.log(typeof score);      //typeof show varible type
+
+//now converstion of vsarible 
+
+let converted =Number(resu);    //Here Number is conver scroe varible type in int  //in resu value was not in number format still convertin number 
+console.log(typeof converted);
+console.log(converted);         //print value is NaN 
+
+
+//for data type Boolean 
+
+//1 ==> true  //0 ==>false
+//" "  ==> false
+
+
+//for string conversion
+
+let numsum =33
+
+let stingnum =String(numsum)  //conver of string
+console.log(typeof stingnum)
