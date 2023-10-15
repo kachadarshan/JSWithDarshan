a37a0acbb6c029d05cf9c51b578a8e7b87a81e55
Kacha Darshan DK98 <112555093+kachadarshan@users.noreply.github.com>
2023-10-15 02:56:20 +0000

    Datatype Summary

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,20 @@
+//here we have to do comparison ==,<=,>= like
+
+// console.log(1<2);
+// console.log(2==2);
+// console.log(1<2);
+// console.log(2 !=1);
+// console.log(2>=1);
+
+
+//when two differrent data types are in comparison that time data type are converted 
+// mdn not allow to compare two different data types 
+
+//console.log("2" > 1); //here string and int are in comparison so here 1 value is converted in string so output is 21
+
+
+// But Solution of this problem is used (===) this comparison called Strically comparison
+
+console.log("2" === 1);  
+
+
---
+++ 01_basic/05_Datatypes_summary.js
@@ -0,0 +1,45 @@
+/* Premitive Data Types  */
+
+// 7 Types of Premitive Data Types 
+// String, Number, Boolean, Null, Undefined, Symobol(used to make value Unique), Bigint
+
+const value1 =100; //Number 
+const value2 =10.25 //Here not FLoat ,Double everything is number 
+
+const isLoggedin = true //its Boolean type
+
+//use of Symbol
+const id ="123"
+const userID ="123"
+
+//in symbol when two value are same in but that result are diffent or unique come 
+
+console.log(id === userID); 
+
+
+
+
+
+/* Refrence(Non Premitive) Data Types */
+
+// Array, Objects, Functions
+
+//arry
+const hero=["Shaktiman","Nagraj","Doga"];
+
+//object write in { object} , and its write in KEY:VALUE pare 
+let myobj =
+{
+    first_name:"Darshan Kacha",
+    age : 25,
+    mobile_number : 8780098183
+}
+
+//Function write used function(){ }
+
+let myFunction= function(){
+    console.log("Its Functionl Tutorial")
+}
+
+
+//typeof used Difine Data Types of parameter we have used  
