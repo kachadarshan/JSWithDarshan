aafc4b53c8f377b10fb2c27874bbccefb71443bd
Kacha Darshan DK98 <112555093+kachadarshan@users.noreply.github.com>
2023-10-16 02:35:10 +0000

    first part of object

---
+++ 02_basic/03_Objects.js
@@ -0,0 +1,33 @@
+/*Objects in javascript its most Important in javascript     */
+
+//Objects Literals
+// const objname ={value of object}   //object write in curly braces its have key,value pair key:value,
+
+const mysymbol1 = Symbol("key1")
+
+const jsuser={ name:"Darshan",
+  age:25,
+  "location":"surat",
+  [mysymbol1]:"Key1",               //this is the right technic to define symbol in object value 
+  email:"darshan.google@gmail.com",
+  logeedin:"yes"
+}
+
+//how to print property of objects --> (.) used this dotmethod and [" "] this method 
+                                // --> but key value have string datatypes use this method [" "]  
+                                
+// console.log(jsuser.age);    
+// console.log(jsuser.email);
+// console.log(jsuser.location);
+// console.log(jsuser[mysymbol1]);     //always symbol key give in [] in the braces this the right way doing this 
+
+
+/*How to Change value of Objects  -------*/
+
+jsuser.email="google@gmai.com"  //this simply override value in object 
+
+/*lock the value means no body can change value use freeze exp */
+Object.freeze(jsuser)     //this is the freeze object means value not the change or override
+jsuser.name="Dax Kacha"    
+
+console.log(jsuser);
