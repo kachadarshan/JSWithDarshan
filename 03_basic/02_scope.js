c2aa164bdf96733e6fff5340101fd63cd38bf19c
Kacha Darshan DK98 <112555093+kachadarshan@users.noreply.github.com>
2023-10-17 11:46:25 +0000

    basic of hoisting

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -3,7 +3,7 @@
     //Local Scope -->value under of {}
     //global scope --> its write outer of {}
 
-    
+
 //-->defination of scope 
 
 //value of between { scope } called scope except Object 
@@ -15,7 +15,49 @@ if(true){        //-->value inside of curly braces called Local Scope and outer 
 
     let val1 ="101"
     const val2="25"
-    console.log(val1)
+    // console.log(val1)
 }
 
-console.log(val1);
+// console.log(val1);
+
+
+
+/*------ Nested Scope ---------- */
+
+function one(){
+
+    const username ="Darshan"
+
+    function two(){         //when nested function happen child used parent property 
+
+        const usercity ="Junagadh"
+        console.log(username)
+    }
+
+    // console.log(usercity);
+
+    two()
+}
+one()
+
+
+
+/* ====++++==== Intresting  ====++++==== */
+
+
+//this style define console.log before function define working fine 
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+//here we store function in variables so we need always define variable first otherwise it show error 
+//simply its Hoisting
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
