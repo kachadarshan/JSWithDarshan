e6a51b7514d830cb5c298d8dd31ee7de8f182cd5
Kacha Darshan DK98 <112555093+kachadarshan@users.noreply.github.com>
2023-10-19 02:19:19 +0000

    while and do while

---
+++ 05_iteration_loop/two_while_dowhileloop.js
@@ -0,0 +1,38 @@
+/*here we learn while and do while loop */
+
+//--->syntax of while loop
+
+ //in while loop we need to decalre varible and increase that variable 
+
+// let index=0;
+// while (condition) {
+//     //your work
+//     index++
+// }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+//---> Do while loop
+
+//-->syntax  in do while its work one time after check
+
+// do {
+    
+// } while (condition);
+
+
+let score=10
+
+do {
+    
+    console.log("Darshan Kacha");
+    score++
+} while (score<=10);
