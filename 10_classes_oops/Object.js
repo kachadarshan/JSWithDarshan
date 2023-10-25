74e80eaa9bdf7e245a6e66f16ebf285476e1fb1a
Kacha Darshan DK98 <112555093+kachadarshan@users.noreply.github.com>
2023-10-25 09:54:11 +0000

    create private method using prototype and how run new keywords

---
+++ 10_classes_oops/Object.js
@@ -0,0 +1,51 @@
+/*In Javascript every thing is an object at the end and object is null in last */
+
+function multipleBy5(num){
+    return num*5
+}
+
+multipleBy5.power = 2
+
+// console.log(multipleBy5(5));
+// console.log(multipleBy5.power);
+// console.log(multipleBy5.prototype);
+
+//--->
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+//in prototype we have to create our methods like increment and printme 
+createUser.prototype.increment = function(){
+    this.score++
+    console.log(this.score);
+}
+createUser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`);   //here this value show us a current context of methods 
+
+}
+
+const chai = new createUser("chai", 25)
+const tea = new createUser("tea", 250)
+
+chai.increment()         //this called created prototype methods 
+
+
+
+//----> Important for interview 
+
+/*
+
+Here's what happens behind the scenes when the new keyword is used:
+
+A new object is created: The new keyword initiates the creation of a new JavaScript object.
+
+A prototype is linked: The newly created object gets linked to the prototype property of the constructor function. This means that it has access to properties and methods defined on the constructor's prototype.
+
+The constructor is called: The constructor function is called with the specified arguments and this is bound to the newly created object. If no explicit return value is specified from the constructor, JavaScript assumes this, the newly created object, to be the intended return value.
+
+The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
+
+*/
